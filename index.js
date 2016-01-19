87261e22cc151990284b85a28966d175c255d10f
Matthew Herbst <herbstmb@miamioh.edu>
2016-01-19 01:12:31 -0800

    conencting to db\!

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var express = require("express"),
-    harp = require("harp");
+var express = require("express");
+var harp = require("harp");
+var Sequelize = require('sequelize');
 
 var app = express();
+var sequelize = new Sequelize('database', 'user', 'pass');
 
 app.use(express.static(__dirname + "/dist"));
 app.use(harp.mount(__dirname + "/dist"));
@@ -9,4 +11,23 @@ app.use(harp.mount(__dirname + "/dist"));
 var port = process.env.PORT || 3333;
 app.listen(port, function() {
   console.log("Listening on port " + port);
+});
+
+var Survey = sequelize.define('survey', {
+  name: Sequelize.STRING
+}, {
+  tableName: 'Surveys'
+});
+
+Survey.sync().then(function() {
+  console.log('Survey table synch success');
+}, function (err) {
+  console.log('Survey table synch failed:', err);
+});
+
+app.get('/get/surveys', function(req, res) {
+  console.log('get/surveys requested');
+
+  res.setHeader('Cache-Control', 'no-cache');
+  res.json(["omg is this going to work?"]);
 });

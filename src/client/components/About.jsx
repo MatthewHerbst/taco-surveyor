bb05e814be5ed336ed5ca3b09a13c973ce05c9cd
Matthew Herbst <herbstmb@miamioh.edu>
2016-01-21 19:11:08 -0800

    fixed typo, more creative text\!

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -10,12 +10,12 @@ module.exports = React.createClass({
       <diw>
         <Row>
           <Col md={8} mdOffset={2}>
-            <p>{'Awesome things happen when Warloards eat their Tacos. Click on \'Surveys\' above to see if you have Bad A** powers.'}</p>
+            <p>{'Awesome things happen when Warlords eat their Tacos. Click on \'Surveys\' above to see if you have Bad A** powers.'}</p>
           </Col>
         </Row>
         <Row>
           <Col md={8} mdOffset={2}>
-            <p>{'Respectively brought to you by Matthew Herbst'}</p>
+            <p>{'Made with a healthy pinch of tortilla, carne, salsa verde, pico de gallo, guacamole, and queso by Matthew Herbst'}</p>
             <p><a href={'https://github.com/MatthewHerbst'}>{'GitHub'}</a></p>
             <p><a href={'stackoverflow.com/users/2518231/MatthewHerbst'}>{'#SOReadyToHelp'}</a></p>
             <p><a href={'https://twitter.com/MattHerbst_'}>{'@MattHerbst_'}</a></p>
